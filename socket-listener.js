009c72220e71218cacfc38403b065dc2548c55b2
Louis Ross <louis.ross@gmail.com>
2022-11-01 15:00:17 -0700

    Interconnects (contains errors)

--- socket-listener.js
+++ socket-listener.js
@@ -7,10 +7,10 @@ const HandleQuery = function(buffer) {
   //var logLine = "Sensor socket injecting offsets ";
   
   var spikes = new Array();
-  const spikeCount = Math.floor((buffer.length - 2) / 8);
+  const spikeCount = Math.floor((buffer.length - 6) / 8);
   //console.log(`Sensor socket injecting ${spikeCount} spikes`);
   // Adjust loop exit to ignore any fragments at the end of the buffer.
-  for (var spikeOffset = 4; spikeOffset < buffer.length - 7; spikeOffset += 8) {
+  for (var spikeOffset = 8; spikeOffset < buffer.length - 7; spikeOffset += 8) {
     const tick = buffer.readInt32LE(spikeOffset);
     const neuronIndex = buffer.readInt32LE(spikeOffset + 4);
     spikes.push([tick, neuronIndex]);
@@ -55,13 +55,13 @@ class PrivateSingleton {
       connection.on('data', function (chunk) {
         if (this._buffer == null) {
           this._bufferSize = chunk.readInt32LE(0);
-          this._bufferSize *= 8;  // Element count (two 4-byte ints per element), make byte count.
+          //this._bufferSize *= 8;  // Element count (two 4-byte ints per element), make byte count.
           //console.log(`Received first chunk of length ${chunk.length} containing buffer size ${this._bufferSize}`);
           this._buffer = Buffer.alloc(0);
         }
 
         this._buffer = Buffer.concat([this._buffer, chunk]);
-        if (this._buffer.length >= this._bufferSize + 4) {
+        if (this._buffer.length >= this._bufferSize /*+ 4*/) {
           const response = JSON.stringify(HandleQuery(this._buffer));
           //this._wsServer.clients.forEach( client => {
           wsServer.clients.forEach( client => {
